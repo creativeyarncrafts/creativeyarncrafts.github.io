124f3cc9d4a1131203da8429a1e2f4bfb45302c0
Creative Yarn Crafts <creativeyarncrafts@gmail.com>
2022-03-26 12:01:58 -0700

    Test Update

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Creative Yarn Crafts
+// Allen P
+// 03/19/2022
+
+// main.js
+
+/**************\
+* social_alert *
+\**************/
+
+// Script for social media links
+
+function social_alert() {
+                   alert(
+      "Social media links are not yet implemented. Please check again in a few months."
+    );
+  }
